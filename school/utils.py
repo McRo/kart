437628fb9423053f2a6e4b76e4f2d741794c9f03
Rojo <romain.lefranc@gmail.com>
2021-01-22 18:04:52 +0100

    WIP : fix add candidatures emails functions

--- school/utils.py
+++ school/utils.py
@@ -2,7 +2,7 @@
 import locale
 import pytz
 from django.utils import timezone
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 from school.models import StudentApplicationSetup
@@ -160,17 +160,54 @@ def send_interview_selection_email_to_candidat(request, candidat, application):
     return mail_sent
 
 
-
 def send_is_on_waitlist_for_interview_to_candidat(request, candidat, application):
+    # set locale  interviews date
+    interviews_dates = {'fr': '', "en": ''}
+    # having name of day/month in rigth language
+    setLocale('fr_FR.utf8')
+    setup = application.campaign
+    interviews_dates['fr'] = "du {0} au {1}".format(setup.interviews_start_date.strftime("%A %d %B"),
+                                                    setup.interviews_end_date.strftime("%A %d %B %Y")
+                                                    )
+    setLocale('en_US.utf8')
+    interviews_dates['en'] = "from {0} to {1}".format(setup.interviews_start_date.strftime("%A %d %B"),
+                                                      setup.interviews_end_date.strftime("%A %d %B %Y")
+                                                      )
+
     # Send email : PRESELECTION : ON WAITLIST
     msg_plain = render_to_string(
         'emails/send_on_waitlist_for_interview_to_candidat.txt',
+        {
+            'application': application,
+            'interviews_dates': interviews_dates
+        }
+    )
+    msg_html = render_to_string(
+        'emails/send_on_waitlist_for_interview_to_candidat.html',
+        {
+            'application': application,
+            'interviews_dates': interviews_dates
+        }
+    )
+    mail_sent = send_mail('Le Fresnoy présélection / preselection',
+                          msg_plain,
+                          'selection@lefresnoy.net',
+                          [candidat.email],
+                          html_message=msg_html,
+                          )
+    return mail_sent
+
+
+def send_is_on_waitlist_for_selection_to_candidat(request, candidat, application):
+    # Send email : PRESELECTION : ON WAITLIST
+    msg_plain = render_to_string(
+        'emails/send_on_waitlist_for_selection_to_candidat.txt',
         {
             'application': application
         }
     )
     msg_html = render_to_string(
-        'emails/send_on_waitlist_for_interview_to_candidat.html',
+        'emails/send_on_waitlist_for_selection_to_candidat.html',
         {
             'application': application
         }
@@ -204,6 +241,50 @@ def send_not_selected_email_to_candidat(request, candidat, application):
                           [candidat.email],
                           html_message=msg_html,
                           )
+    return mail_sent
+
+
+def send_candidature_not_finalized_to_candidats(request, application_setup, list_candidats):
+
+    # jeudi 22 avril 2021 à 14h
+    application = application_setup.applications
+    # set locale  interviews date
+    application_end = {'fr': '', "en": ''}
+    # having name of day/month in rigth language
+    setLocale('fr_FR.utf8')
+    tz = pytz.timezone("Europe/Paris")
+    candidature_date_end = application_setup.candidature_date_end.astimezone(tz)
+
+    application_end['fr'] = '{0} à {1}'.format(
+        candidature_date_end.strftime("%A %d %B %Y"),
+        candidature_date_end.strftime("%Hh%M")
+    )
+    setLocale('en_US.utf8')
+    application_end['en'] = candidature_date_end.strftime("%A %d %B %Y at %I.%M %p")
+    # Send email : NOT SELECTED
+    msg_plain = render_to_string(
+        'emails/send_candidature_not_finalized_to_candidat.txt',
+        {
+            'application': application,
+            'application_end': application_end
+        }
+    )
+    msg_html = render_to_string(
+        'emails/send_candidature_not_finalized_to_candidat.html',
+        {
+            'application': application,
+            'application_end': application_end
+        }
+    )
+    mail = EmailMultiAlternatives('Candidatre non finalisée / Application not finalized',
+                                  msg_plain,
+                                  'selection@lefresnoy.net',
+                                  ['selection@lefresnoy.net', ],
+                                  bcc=[list_candidats],  # bcc
+                                  reply_to=['selection@lefresnoy.net'],
+                                  )
+    mail.attach_alternative(msg_html, "text/html")
+    mail_sent = mail.send()
     return mail_sent
 
 
